400c42d40841bc9dfb4ae2cf45005495688d8600
Felix Garcia Lainez <fgarcialainez@gmail.com>
2023-02-19 14:12:46 +0100

    Implemented some adjustments in the frontend service. Included k8s configuration files for the frontend service and the ingress.

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -36,7 +37,13 @@ func render(w http.ResponseWriter, t string) {
 		return
 	}
 
-	if err := tmpl.Execute(w, nil); err != nil {
+	var data struct {
+		BrokerURL string
+	}
+
+	data.BrokerURL = os.Getenv("BROKER_URL")
+
+	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
